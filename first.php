5858c8f1e7e8f95cb57c02c029cb3efaaf25bb5c
mrx-arafat <arafatmrx@gmail.com>
2022-05-18 08:55:08 +0600

    PHP Files

---
+++ first.php
@@ -0,0 +1,23 @@
+<?php
+
+$msg= "heyy ";
+
+$name= "arafat"; 
+echo $msg;
+
+echo $name;
+echo " <h2> mara khau</h2> ";
+
+$one="text test";
+$two=123;
+$three=72.3;
+$four =true;
+$five =array(123,'No time to die',69.99);
+$six=NULL;
+
+echo var_dump($five);
+
+
+
+
+?>

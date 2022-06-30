c587b13ea062a2a83a7afc9dd24dcc5e471cfc9d
mrx-arafat <arafatmrx@gmail.com>
2022-06-30 17:27:53 +0600

    basic php

---
+++ function.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+function print_num($n){
+
+
+    echo "<br> your number is :";
+    echo $n;
+}
+
+
+print_num(68);
+print_num(22);
+print_num(53);
+print_num(43);
+
+
+?>
+
+</body>
+</html>
